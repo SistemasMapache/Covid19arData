90ec34a6399a43ee937eb81a7e662772cd3541a1
Vladimiro Bellini <sonido@gmail.com>
2020-04-20 18:11:43 -0300

    Update script_post.py

--- _scripts/script_post.py
+++ _scripts/script_post.py
@@ -1,8 +1,6 @@
 # https://gspread.readthedocs.io/en/latest/
 import gspread
-
 import time
-
 from google.oauth2.service_account import Credentials
 from datetime import date, timedelta
 
@@ -26,16 +24,17 @@ provs1 = wks2.col_values(2)
 provs2 = wks2.col_values(3)
   
 yesterday = date.today() - timedelta(days=1)
+today     = date.today().strftime('%d/%m/%Y')
 yesterday = yesterday.strftime('%d/%m/%Y')
+
+fechacol1 = yesterday
 
 #insert por provincia
 def insert (prov,casos,fallecidos):
-    print(prov+' start')
-
-    # constraint google api tope cada 10 segundos
-    time.sleep(1)
+    print(prov+' strt')
 
     # ultima posicion para insert
+    time.sleep(1)
     allvalues = wks1.get_all_values()
     lastrow = len(allvalues)
 
@@ -43,30 +42,55 @@ def insert (prov,casos,fallecidos):
     nextrow = lastrow+1
     
     # inserta predet
-    wks1.update_cell(nextrow,  1, yesterday)
+    time.sleep(1)
+    wks1.update_cell(nextrow,  1, fechacol1)
+    time.sleep(1)
     wks1.update_cell(nextrow,  2, '=DAYS(A408;FECHA(2020;3;4))')
+    time.sleep(1)
     wks1.update_cell(nextrow,  3, '=DAYS(A408;FECHA(2020;3;20))')
+    time.sleep(1)
     wks1.update_cell(nextrow,  4, 'Argentina')
     
     # tot_casosconf
+    time.sleep(1)
     wks1.update_cell(nextrow,  7, '=G'+str(lastrow)+'+H'+str(nextrow))
     
     # nue_casosconf_diff
+    time.sleep(1)
     wks1.update_cell(nextrow,  8, casos)
     
     # tot_fallecidos
+    time.sleep(1)
     wks1.update_cell(nextrow,  9, '=I'+str(lastrow)+'+J'+str(nextrow))
     
     # nue_fallecidos_diff
+    time.sleep(1)
     wks1.update_cell(nextrow, 10, fallecidos)
 
     # formato string provs
     prov1pos = provs1.index(prov)
+    # post
+    time.sleep(1)
     wks1.update_cell(nextrow,  5, provs1[prov1pos])
+    time.sleep(1)
     wks1.update_cell(nextrow, 18, provs2[prov1pos])
     
-    print(prov+' end')
+    print(prov+' nd')
     #end
 
 # ejemplo
-# insert('Buenos Aires',5,1)
+# insert('Buenos Aires',3,1)
+# usar casos.csv. chequear fallecidos. correr.
+# ej insert('Buenos Aires', 30, 3 )
+
+ ## 7 nuevas muertes. Todos hombres; tres de ellos, de 54, 79 y 92 años, residentes en la Provincia de Buenos Aires,
+ # uno de 85 años residente en Mendoza; otros dos, de 86 y 95 años residentes en la CABA 
+
+# extrae casos
+with open('casos.csv') as f:
+    lines = [line.rstrip() for line in f]
+
+for line in lines:
+    prep = line.split(",")
+    print(prep)
+    insert( prep[0] , int(prep[1]) , int(prep[2]) )
