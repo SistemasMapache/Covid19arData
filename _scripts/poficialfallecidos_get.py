66edee47234b9e66562db07fd33c428c48615fc4
Vladimiro Bellini <sonido@gmail.com>
2020-04-21 02:55:40 -0300

    Create poficialfallecidos_get.py

---
+++ _scripts/poficialfallecidos_get.py
@@ -0,0 +1,30 @@
+# needs pdftotext
+# pip install requests textract pandas bs4
+
+import requests
+import textract
+import re
+import pandas as pd
+from bs4 import BeautifulSoup, SoupStrainer
+
+
+# extrae casos
+with open('parte.txt') as infile, open('fallecidos.txt', 'w') as outfile:
+    copy = False
+    for line in infile:
+        if line.strip().startswith('Se registraron'):
+            copy = True
+            outfile.write(str(line))
+            continue
+        elif line.strip().startswith("Del total de casos"):
+            copy = False
+            continue
+        elif line.strip().startswith("A la fecha"):
+            copy = False
+            continue
+        elif line.strip().startswith("Detalle por provincia"):
+            copy = False
+            continue
+        elif copy:
+            if str(line) != '':
+                outfile.write(str(line))
